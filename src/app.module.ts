68a83d0f6cc835465616e1455e535b7cecf06c66
alexcastrodev <alexoliveira7x@gmail.com>
2025-11-27 00:01:34 +0000

    remove user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './module/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { databaseConfig } from './config/database';
-import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SequelizeModule.forRoot(databaseConfig), UserModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
